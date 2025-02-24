462255a945c0e240c8bdcf3d7f38c75c2ef36ead
GS8LIFE <gs.8life@gmail.com>
2025-02-24 20:57:48 +0900

    조선욱
    조선욱 : ignore 정상적용

--- TPS_Project/Source/TPS_Project/TPS_Project.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TPS_Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TPS_Project, "TPS_Project" );
