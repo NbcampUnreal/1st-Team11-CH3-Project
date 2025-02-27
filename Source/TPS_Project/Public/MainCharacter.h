d8e7afa0166cad3eb73724153d3f957ff3594bb3
robertobbaik <robertobbaik@gmail.com>
2025-02-27 19:06:59 +0900

    [Update] 격발 중 타격 시 격발 종료
    [Update] 격발 중 타격 시 격발 종료

--- Source/TPS_Project/Public/MainCharacter.h
+++ Source/TPS_Project/Public/MainCharacter.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -16,89 +15,86 @@ struct FInputActionValue;
 UCLASS()
 class TPS_PROJECT_API AMainCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	AMainCharacter();
+    AMainCharacter();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+    UCameraComponent* Camera;
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+    USpringArmComponent* SpringArm;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-	UCameraComponent* Camera;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-	USpringArmComponent* SpringArm;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-	UStaticMeshComponent* StaticMesh;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+    UStaticMeshComponent* StaticMesh;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Particle")
     UNiagaraComponent* MuzzleFlash;
 
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-	float NormalSpeed;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Animation")
-	UAnimMontage* FireMontage;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+    float NormalSpeed;
 
-	UPROPERTY(EditDefaultsOnly, Category="Animation")
-	UAnimMontage* HitMontage;
+    UPROPERTY(EditDefaultsOnly, Category = "Animation")
+    UAnimMontage* FireMontage;
 
-	UPROPERTY(EditDefaultsOnly, Category="Animation")
-	UAnimMontage* ReloadMontage;
+    UPROPERTY(EditDefaultsOnly, Category = "Animation")
+    UAnimMontage* HitMontage;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Animation")
-	bool IsFire;
+    UPROPERTY(EditDefaultsOnly, Category = "Animation")
+    UAnimMontage* ReloadMontage;
 
-	FTimerHandle FireTimer;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation")
+    bool bIsFire;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation")
+    bool bHasDamage;
+
+    FTimerHandle FireTimer;
+
+
 
 public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    UFUNCTION()
+    void Move(const FInputActionValue& Value);
+    UFUNCTION()
+    void Look(const FInputActionValue& Value);
+    UFUNCTION()
+    void StartJump(const FInputActionValue& Value);
+    UFUNCTION()
+    void StopJump(const FInputActionValue& Value);
+    UFUNCTION()
+    void GunFire(const FInputActionValue& Value);
+    UFUNCTION()
+    void StopGunFire(const FInputActionValue& Value);
 
-	UFUNCTION()
-	void Move(const FInputActionValue& Value);
-	UFUNCTION()
-	void Look(const FInputActionValue& Value);
-	UFUNCTION()
-	void StartJump(const FInputActionValue& Value);
-	UFUNCTION()
-	void StopJump(const FInputActionValue& Value);
-	UFUNCTION()
-	void GunFire(const FInputActionValue& Value);
-	UFUNCTION ()
-	void StopGunFire (const FInputActionValue& Value);
+    UFUNCTION()
+    void Reload(const FInputActionValue& Value);
 
-	UFUNCTION()
-	void Reload(const FInputActionValue& Value);
-
-	UFUNCTION(BlueprintCallable)
-	void PlayDamageAnim();
+    UFUNCTION(BlueprintCallable)
+    void PlayDamageAnim();
 
     virtual float TakeDamage(float DamageAmount,
-        struct FDamageEvent const &DamageEvent,
-        AController *EventInstigator,
-        AActor *DamageCauser) override;
+        struct FDamageEvent const& DamageEvent,
+        AController* EventInstigator,
+        AActor* DamageCauser) override;
 
-	float GetCharacterHealth() const;
+    float GetCharacterHealth() const;
 
-	void SetCharacterHealth(float Value);
-	
-	void InitAnimation();
+    void SetCharacterHealth(float Value);
 
-	void Fire();
-	void GunShotAnimation();
-	void ActivateMuzzle();
+    void Fire();
+    void GunShotAnimation();
+    void ActivateMuzzle();
+    void SetDamageState(bool HasDamage);
+
 private:
-	float Health;
-	int Ammo;
+    float Health;
+    int Ammo;
 };
