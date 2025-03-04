e597af4e4d0ed1416b429b7934567d629b305d75
UnBark <98524391+UnBark@users.noreply.github.com>
2025-03-04 16:29:18 +0900

    총기 시스템
    에러로 인해 재업
    추가 : 탄약 개수 및 재장전 기능

---
+++ Source/TPS_Project/Private/MainWeapon.cpp
@@ -0,0 +1,87 @@
+#include "MainWeapon.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
+
+AMainWeapon::AMainWeapon()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    // 스태틱 메시 생성
+    WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+    RootComponent = WeaponMesh;
+}
+
+void AMainWeapon::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // 머터리얼 적용
+    if (WeaponMesh && WeaponMaterial)
+    {
+        WeaponMesh->SetMaterial(0, WeaponMaterial);
+    }
+}
+
+void AMainWeapon::Fire()
+{
+    if (AmmoCount > 0 && bCanFire)
+    {
+        PerformLineTrace();
+        AmmoCount--;
+        UE_LOG(LogTemp, Warning, TEXT("남은 탄약 : %d"), AmmoCount);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("탄약 없음! 재장전하세요."));
+    }
+}
+
+void AMainWeapon::PerformLineTrace()
+{
+    FVector Start = WeaponMesh->GetComponentLocation();
+    FVector ForwardVector = WeaponMesh->GetForwardVector();
+    FVector End = Start + (ForwardVector * Range);
+
+    FHitResult HitResult;
+    FCollisionQueryParams Params;
+    Params.AddIgnoredActor(this);
+
+    bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, Params);
+
+    if (bHit)
+    {
+        UGameplayStatics::ApplyPointDamage(
+            HitResult.GetActor(),
+            Damage,
+            ForwardVector,
+            HitResult,
+            GetInstigatorController(),
+            this,
+            nullptr
+        );
+    }
+
+    // 디버그 라인 표시
+    if (bDebugLine)
+    {
+        DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 1.0f, 0, 2.0f);
+    }
+}
+
+void AMainWeapon::Reload()
+{
+    if (AmmoCount < MaxAmmo)
+    {
+        bCanFire = false;
+        UE_LOG(LogTemp, Warning, TEXT("재장전 중..."));
+
+        GetWorld()->GetTimerManager().SetTimer(ReloadTimer, this, &AMainWeapon::FinishReload, ReloadTime, false);
+    }
+}
+
+void AMainWeapon::FinishReload()
+{
+    AmmoCount = MaxAmmo;
+    bCanFire = true;
+    UE_LOG(LogTemp, Warning, TEXT("재장전 완료, 탄약 : %d"), AmmoCount);
+}
